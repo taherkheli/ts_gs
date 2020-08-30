ab9cfe349ca2d3249689be2ab91f8eb162ad7b4d
bilal taherkheli <bilal76@gmail.com>
2020-08-30 20:01:15 +0200

    TS installed, configured, and dev env set for running n debugging

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-  entry: './dist/app.js',
+  entry: './src/app/app.ts',
   devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
