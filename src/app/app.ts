86b37a347830346f7a4c3c9c7c3d77b3b40552e4
Bilal Taherkheli <bilal76@gmail.com>
2020-08-31 17:48:01 +0200

    mod4 finished. Functions, function type, arrow functions

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,52 @@
 function startGame() {
     // starting a new game
 
-    let playerName : string = 'Audrey';
+    let playerName : string | undefined = getInputValue("playername");
     logPlayer(playerName);
-
-    let messageElement : HTMLElement | null = document.getElementById('messages');
-    messageElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+    postScore(80, playerName);
+    postScore(-5, playerName);
+    // let messageElement : HTMLElement | null = document.getElementById('messages');
+    // messageElement!.innerText = 'Welcome to MultiMath! Starting new game...';
 }
 
-function logPlayer(name :string) {
+function logPlayer(name : string = 'MultiMath Player'): void{
     console.log(`New game starting for player: ${name}`);
 }
 
+function getInputValue(elementId : string) : string | undefined {
+    const inputElement : HTMLInputElement = <HTMLInputElement>document.getElementById(elementId);
+
+    if (inputElement.value === '')
+        return undefined;
+    else
+        return inputElement.value;
+}
+
+function postScore(score : number, playerName : string = 'MultiMath Player'): void {
+
+    let logger : (value : string) => void; 
+
+    if(score < 0){
+        logger = logError;
+    }
+    else{
+        logger = logMessage;
+    }
+
+    const scoreElement : HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;    
+
+    logger(`Score: ${score}`)
+}
+
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+//converting this traditional function to an arrow function
+// function logMessage(message : string): void {
+//     console.log((message));
+// }
+const logMessage =  (message : string) : void => console.log(message);
+
+function logError(err : string): void {
+    console.error(err);
+}
