e86fdf359f0090294a11ca8bda23c5da59dba6c6
Bilal Taherkheli <bilal@danfoss.com>
2020-08-31 13:48:28 +0200

    Mod 3 done. Type safety and type inference features provided by tsc

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,15 @@
 function startGame() {
-    var messageElement = document.getElementById('messages');
+    // starting a new game
+
+    let playerName : string = 'Audrey';
+    logPlayer(playerName);
+
+    let messageElement : HTMLElement | null = document.getElementById('messages');
     messageElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+}
+
+function logPlayer(name :string) {
+    console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
