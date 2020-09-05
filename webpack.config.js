e9fa221594f9ba442e1d52ff87bea3736c34590c
bilal taherkheli <bilal76@gmail.com>
2020-09-05 14:53:36 +0200

    Mod 6 finished. ALl about module creation, consumption, and resolving

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  //entry: './src/app/app.ts',
-  entry: './dist/app.js',
-  // devtool: 'inline-source-map',
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.tsx?$/,
-  //       use: 'ts-loader',
-  //       exclude: /node_modules/
-  //     }
-  //   ]
-  // },
-  // resolve: {
-  //   extensions: [ '.tsx', '.ts', '.js' ]
-  // },
-  // output: {
-  //   filename: 'bundle.js'
-  // },
+  entry: './src/app/app.ts',
+  //entry: './dist/app.js',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: './src/bundle.js'
+  },
   devServer: {
     inline: false
   }
